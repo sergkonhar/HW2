2af07266f3c55d70955067081b3e8cb5106b63e0
sergkonhar <sergkonhar@gmail.com>
2018-10-09 22:59:31 +0300

    8 tasks are done and  9 task is started here

--- src/Task9_0.java
+++ src/Task9_0.java
@@ -12,15 +12,106 @@ public class Task9_0 {
            int a = Integer.parseInt(marixSize.split("x")[0]);
            int b = Integer.parseInt(marixSize.split("x")[1]);
            System.out.println("\n" + "Input the maximal number");
-           int maxValue = size.nextInt();
+           int maxValue = size.nextInt()+1;
+           System.out.println("\n" + "Input the minimal number");
+           int minValue = size.nextInt();
+           System.out.println("\n" + "Input the amount of positions to shift");
+           int shift = size.nextInt();
 
            Random nRand = new Random();
-           int [][] randomArray = new int[a][b];
-           for (int i = 0; i <a ; i++) {
-               for (int j = 0; j <b ; j++) {
-                   randomArray[i][j]= nRand.nextInt(maxValue)+1;
+           int[][] randomArray = new int[a][b];
+           for (int i = 0; i < a; i++) {
+               for (int j = 0; j < b; j++) {
+                   randomArray[i][j] = minValue + (nRand.nextInt((maxValue )) * 2);
                }
            }
+           for (int i = 0; i < a; i++) {
+               for (int j = 0; j < b; j++) {
+                   System.out.print(randomArray[i][j] + " ");
+               }
+               System.out.println();
+           }
+
+           int helper = 0;
+           for (int i = 0; i < b; i++) {
+               for (int j = a; j >= 0; j--) {
+                   for (int k = 0; k < j - 1; k++) {
+                       if (randomArray[i][k] > randomArray[i][k + 1]) {
+                           helper = randomArray[i][k];
+                           randomArray[i][k] = randomArray[i][k + 1];
+                           randomArray[i][k + 1] = helper;
+
+                       }
+                   }
+               }
+           }
+
+           System.out.println("\n" + "array sorted in horizontal plane");
+           for (int i = 0; i < a; i++) {
+               for (int j = 0; j < b; j++) {
+                   System.out.print(randomArray[i][j] + " ");
+               }
+               System.out.println();
+           }
+           int helper1 = 0;
+           for (int i = 0; i < b; i++) {
+               for (int j = a; j >= 0; j--) {
+                   for (int k = 0; k < j - 1; k++) {
+                       if (randomArray[k][i] > randomArray[k + 1][i]) {
+                           helper1 = randomArray[k][i];
+                           randomArray[k][i] = randomArray[k + 1][i];
+                           randomArray[k + 1][i] = helper1;
+
+                       }
+                   }
+               }
+           }
+           System.out.println("\n" + "array sorted in vertical plane");
+           for (int i = 0; i < a; i++) {
+               for (int j = 0; j < b; j++) {
+                   System.out.print(randomArray[i][j] + " ");
+               }
+               System.out.println();
+           }
+
+           for (int s = 0; s <shift ; s++) {
+
+
+           for (int i = 0; i < a; i++) {
+               int count = 0;
+               int h1 = 0;
+               int h2 = 0;
+               for (int j = 0; j < b; j++) {
+                   count += 1;
+                   if (j == b - 1 & count % 2 != 0) {
+                       randomArray[i][0] = h1;
+                   }
+                   if (j == b - 1 & count % 2 == 0) {
+                       randomArray[i][0] = h2;
+                   }
+                   if (count % 2 != 0 & j != b - 1 & count == 1) {
+                       h1 = randomArray[i][j];
+                       h2 = randomArray[i][j + 1];
+                       randomArray[i][j + 1] = h1;
+
+                   }
+                   if (count % 2 != 0 & j != b - 1 & count != 1) {
+
+                       h2 = randomArray[i][j + 1];
+                       randomArray[i][j + 1] = h1;
+                       h1 = randomArray[i][j];
+                   }
+                   if (count % 2 == 0 & j != b - 1) {
+                       h1 = randomArray[i][j + 1];
+
+                       randomArray[i][j + 1] = h2;
+                       h2 = randomArray[i][j];
+                   }
+
+               }
+           }
+       }
+           System.out.println("\n"+"Shifted to the right by"+" "+shift+" "+ "positions");
            for (int i = 0; i <a ; i++) {
                for (int j = 0; j <b ; j++) {
                    System.out.print(randomArray[i][j]+" ");
@@ -29,6 +120,12 @@ public class Task9_0 {
            }
 
 
+           System.out.println("\n"+"Loop");
+
+               for (int j = 0; j <b ; j++) {
+                   System.out.print((j+1)%b);
+               }
+               System.out.println();
 
 
        }
