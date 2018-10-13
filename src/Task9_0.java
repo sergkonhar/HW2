5af69a7ab2060a346c1278907cb7e6a23e092b6a
sergkonhar <sergkonhar@gmail.com>
2018-10-13 17:20:04 +0300

    9 task with shifting is done here

--- src/Task9_0.java
+++ src/Task9_0.java
@@ -12,7 +12,7 @@ public class Task9_0 {
            int a = Integer.parseInt(marixSize.split("x")[0]);
            int b = Integer.parseInt(marixSize.split("x")[1]);
            System.out.println("\n" + "Input the maximal number");
-           int maxValue = size.nextInt()+1;
+           int maxValue = size.nextInt() + 1;
            System.out.println("\n" + "Input the minimal number");
            int minValue = size.nextInt();
            System.out.println("\n" + "Input the amount of positions to shift");
@@ -22,7 +22,7 @@ public class Task9_0 {
            int[][] randomArray = new int[a][b];
            for (int i = 0; i < a; i++) {
                for (int j = 0; j < b; j++) {
-                   randomArray[i][j] = minValue + (nRand.nextInt((maxValue )) * 2);
+                   randomArray[i][j] = minValue + (nRand.nextInt((maxValue - minValue)));
                }
            }
            for (int i = 0; i < a; i++) {
@@ -74,61 +74,248 @@ public class Task9_0 {
                System.out.println();
            }
 
-           for (int s = 0; s <shift ; s++) {
+           for (int s = 0; s < shift; s++) {
 
 
+               for (int i = 0; i < a; i++) {
+                   int count = 0;
+                   int h1 = 0;
+                   int h2 = 0;
+                   for (int j = 0; j < b; j++) {
+                       count += 1;
+                       if (j == b - 1 & count % 2 != 0) {
+                           randomArray[i][0] = h1;
+                       }
+                       if (j == b - 1 & count % 2 == 0) {
+                           randomArray[i][0] = h2;
+                       }
+                       if (count % 2 != 0 & j != b - 1 & count == 1) {
+                           h1 = randomArray[i][j];
+                           h2 = randomArray[i][j + 1];
+                           randomArray[i][j + 1] = h1;
+
+                       }
+                       if (count % 2 != 0 & j != b - 1 & count != 1) {
+
+                           h2 = randomArray[i][j + 1];
+                           randomArray[i][j + 1] = h1;
+                           h1 = randomArray[i][j];
+                       }
+                       if (count % 2 == 0 & j != b - 1) {
+                           h1 = randomArray[i][j + 1];
+
+                           randomArray[i][j + 1] = h2;
+                           h2 = randomArray[i][j];
+                       }
+
+                   }
+               }
+           }
+           System.out.println("\n" + "Shifted to the right by" + " " + shift + " " + "positions");
            for (int i = 0; i < a; i++) {
-               int count = 0;
-               int h1 = 0;
-               int h2 = 0;
                for (int j = 0; j < b; j++) {
-                   count += 1;
-                   if (j == b - 1 & count % 2 != 0) {
-                       randomArray[i][0] = h1;
-                   }
-                   if (j == b - 1 & count % 2 == 0) {
-                       randomArray[i][0] = h2;
-                   }
-                   if (count % 2 != 0 & j != b - 1 & count == 1) {
-                       h1 = randomArray[i][j];
-                       h2 = randomArray[i][j + 1];
-                       randomArray[i][j + 1] = h1;
-
-                   }
-                   if (count % 2 != 0 & j != b - 1 & count != 1) {
-
-                       h2 = randomArray[i][j + 1];
-                       randomArray[i][j + 1] = h1;
-                       h1 = randomArray[i][j];
-                   }
-                   if (count % 2 == 0 & j != b - 1) {
-                       h1 = randomArray[i][j + 1];
-
-                       randomArray[i][j + 1] = h2;
-                       h2 = randomArray[i][j];
-                   }
-
-               }
-           }
-       }
-           System.out.println("\n"+"Shifted to the right by"+" "+shift+" "+ "positions");
-           for (int i = 0; i <a ; i++) {
-               for (int j = 0; j <b ; j++) {
-                   System.out.print(randomArray[i][j]+" ");
+                   System.out.print(randomArray[i][j] + " ");
                }
                System.out.println();
            }
 
+           int[][] secondArray = new int[a][b];
+           int additionalCounter = 1;
+           for (int k = 0; k < shift * 2; k++) {
 
-           System.out.println("\n"+"Loop");
+               if (additionalCounter % 2 != 0) {
+                   for (int i = 0; i < a; i++) {
+                       for (int j = 0; j < b; j++) {
+                           secondArray[i][j] = randomArray[i][(j + 1) % b];
+                       }
+                   }
 
-               for (int j = 0; j <b ; j++) {
-                   System.out.print((j+1)%b);
                }
-               System.out.println();
+               if (additionalCounter % 2 == 0) {
+                   for (int i = 0; i < a; i++) {
+                       for (int j = 0; j < b; j++) {
+                           randomArray[i][j] = secondArray[i][(j + 1) % b];
+                       }
+                   }
+
+               }
+
+               additionalCounter += 1;
+           }
 
 
-       }
+           System.out.println("Array shifted left by" + "  " + shift + "  " + "positions");
+           if (additionalCounter % 2 != 0) {
+               for (int i = 0; i < a; i++) {
+                   for (int j = 0; j < b; j++) {
+                       System.out.print(randomArray[i][j] + "  ");
+
+                   }
+                   System.out.println();
+               }
+           }
+           if (additionalCounter % 2 == 0) {
+               for (int i = 0; i < a; i++) {
+                   for (int j = 0; j < b; j++) {
+                       System.out.print(secondArray[i][j] + "  ");
+
+                   }
+                   System.out.println();
+               }
+           }
+           int newcounter = 1;
+           for (int i = 0; i < shift; i++) {
+               if (additionalCounter % 2 != 0) {
+                   if (newcounter % 2 != 0) {
+                       for (int j = 0; j < b; j++) {
+                           for (int k = 0; k < a; k++) {
+                               secondArray[k][j] = randomArray[(k + 1) % a][j];
+                           }
+                       }
+                   }
+                   if (newcounter % 2 == 0) {
+                       for (int j = 0; j < b; j++) {
+                           for (int k = 0; k < a; k++) {
+                               randomArray[k][j] = secondArray[(k + 1) % a][j];
+                           }
+                       }
+                   }
+
+
+               }
+               if (additionalCounter % 2 == 0) {
+                   if (newcounter % 2 != 0) {
+                       for (int j = 0; j < b; j++) {
+                           for (int k = 0; k < a; k++) {
+                               randomArray[k][j] = secondArray[(k + 1) % a][j];
+                           }
+                       }
+                   }
+                   if (newcounter % 2 == 0) {
+                       for (int j = 0; j < b; j++) {
+                           for (int k = 0; k < a; k++) {
+                               secondArray[k][j] = randomArray[(k + 1) % a][j];
+                           }
+                       }
+                   }
+
+
+               }
+               newcounter += 1;
+
+
+           }
+
+
+           System.out.println("Array shifted up by" + "  " + shift + "  " + "positions");
+           if (newcounter % 2 != 0) {
+               for (int i = 0; i < a; i++) {
+                   for (int j = 0; j < b; j++) {
+                       System.out.print(randomArray[i][j] + "  ");
+
+                   }
+                   System.out.println();
+               }
+           }
+           if (newcounter % 2 == 0) {
+               for (int i = 0; i < a; i++) {
+                   for (int j = 0; j < b; j++) {
+                       System.out.print(secondArray[i][j] + "  ");
+
+                   }
+                   System.out.println();
+               }
+           }
+           int onemorecounter = 1;
+           for (int i = 0; i < shift * 2; i++) {
+               if (newcounter % 2 != 0) {
+                   if (onemorecounter % 2 != 0) {
+                       for (int j = 0; j < b; j++) {
+                           for (int k = 0; k < a; k++) {
+                               secondArray[(k + 1)%a][j] = randomArray[k][j];
+                           }
+                       }
+                   }
+                   if (onemorecounter % 2 == 0) {
+                       for (int j = 0; j < b; j++) {
+                           for (int k = 0; k < a; k++) {
+                               randomArray[(k + 1)%a][j] = secondArray[k][j];
+                           }
+                       }
+                   }
+
+
+               }
+               if (newcounter % 2 == 0) {
+                   if (onemorecounter % 2 != 0) {
+                       for (int j = 0; j < b; j++) {
+                           for (int k = 0; k < a; k++) {
+                               randomArray[(k + 1)%a][j] = secondArray[k][j];
+                           }
+                       }
+                   }
+                   if (onemorecounter % 2 == 0) {
+                       for (int j = 0; j < b; j++) {
+                           for (int k = 0; k < a; k++) {
+                               secondArray[(k + 1)%a][j] = randomArray[k][j];
+                           }
+                       }
+                   }
+
+
+               }
+               onemorecounter += 1;
+
+
+           }
+           System.out.println("Array shifted  by" + "  " + shift + "  " + "positions");
+          if (newcounter%2==0) {
+              if (onemorecounter % 2 != 0) {
+                  for (int i = 0; i < a; i++) {
+                      for (int j = 0; j < b; j++) {
+                          System.out.print(secondArray[i][j] + "  ");
+
+                      }
+                      System.out.println();
+
+                  }
+              }
+              if (onemorecounter % 2 == 0) {
+                  for (int i = 0; i < a; i++) {
+                      for (int j = 0; j < b; j++) {
+                          System.out.print(randomArray[i][j] + "  ");
+
+                      }
+                      System.out.println();
+                  }
+
+
+              }
+          }
+           if (newcounter%2!=0) {
+               if (onemorecounter % 2 != 0) {
+                   for (int i = 0; i < a; i++) {
+                       for (int j = 0; j < b; j++) {
+                           System.out.print(randomArray[i][j] + "  ");
+
+                       }
+                       System.out.println();
+                   }
+               }
+               if (onemorecounter % 2 == 0) {
+                   for (int i = 0; i < a; i++) {
+                       for (int j = 0; j < b; j++) {
+                           System.out.print(secondArray[i][j] + "  ");
+
+                       }
+                       System.out.println();
+                   }
+
+
+               }
+           }
+          }
+
     catch (Exception e){
         System.out.println("Wrong format. Potentially there is a wrong separator" + "  "+e );
     }
